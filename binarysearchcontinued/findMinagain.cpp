d26b8e78f2fce87c89786505696b6a6798bf04f8
turtlebeasts <179turtleshells@gmail.com>
2026-01-16 18:07:40 +0530

    practice

--- binarysearchcontinued/findMinagain.cpp
+++ binarysearchcontinued/findMinagain.cpp
@@ -1,1 +1,23 @@
-// will do this later
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {3, 4, 5, 1, 2};
+    int i = 0, j = nums.size() - 1;
+    while (i < j)
+    {
+        int mid = i + (j - i) / 2;
+        if (nums[mid] > nums[j])
+        {
+            i = mid + 1;
+        }
+        else
+        {
+            j = mid;
+        }
+    }
+    cout << nums[i];
+    return 0;
+}
