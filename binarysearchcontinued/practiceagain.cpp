51b9d1f7c7a77b1b11d1e8d93da848ddfe6cc069
turtlebeasts <179turtleshells@gmail.com>
2026-01-16 18:40:24 +0530

    learn when to use <= and < sign

---
+++ binarysearchcontinued/practiceagain.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+
+    int i = 0, j = nums.size() - 1;
+
+    // <= since we are searching for element
+    while (i <= j)
+    {
+        int mid = i + (j - i) / 2;
+        if (nums[mid] == target)
+        {
+            cout << mid;
+            return 0;
+        }
+
+        if (nums[i] <= nums[mid])
+        {
+            if (nums[i] <= target && target < nums[mid])
+            {
+                j = mid - 1;
+            }
+            else
+            {
+                i = mid + 1;
+            }
+        }
+        else
+        {
+            if (nums[mid] < target && target <= nums[j])
+            {
+                i = mid + 1;
+            }
+            else
+            {
+                j = mid - 1;
+            }
+        }
+    }
+    cout << "Not found";
+    return 0;
+}
