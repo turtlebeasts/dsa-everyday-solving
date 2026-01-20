5e67c5aab62ff10282300a8e8fe1495b7813984e
turtlebeasts <179turtleshells@gmail.com>
2026-01-20 11:38:22 +0530

    failed linked list

--- tree/test.cpp
+++ tree/test.cpp
@@ -15,15 +15,38 @@ struct Node
 int main()
 {
     Node *head = new Node(10);
-    head->next = new Node(20);
-    head->next->next = new Node(30);
-    head->next->next->next = new Node(40);
-    head->next->next->next->next = new Node(50);
+    // head->next = new Node(20);
+    // head->next->next = new Node(30);
+    // head->next->next->next = new Node(40);
+    // head->next->next->next->next = new Node(50);
 
-    while (head != NULL)
+    int n = 0;
+    int length = 0;
+    int nth = 0;
+    Node *temp = head;
+    // find the length
+    while (temp != NULL)
     {
-        cout << head->val << " -> ";
-        head = head->next;
+        length++;
+        temp = temp->next;
+    }
+
+    temp = head;
+    while (length - nth - 1 != n)
+    {
+        nth++;
+        temp = temp->next;
+    }
+
+    temp->next = temp->next->next;
+    temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val;
+        if (temp->next != NULL)
+            cout << "->";
+        temp = temp->next;
     }
     return 0;
 }
