93d45a4643133b9a8957066bad37b97beb508b41
Mriganka <52573759+turtlebeasts@users.noreply.github.com>
2026-02-07 11:08:01 +0530

    Time: 11 ms (27.77%), Space: 104.2 MB (25.19%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -2,36 +2,35 @@ class Solution {
 public:
     int n, m;
     vector<vector<int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
-    int paremeter = 0;
 
-    void dfs(int r, int c, vector<vector<int>>& grid){
+    int dfs(int r, int c, vector<vector<int>>& grid){
         if(r<0 || c<0 || r>=n || c>=m){
-            paremeter++;
-            return;
+            return 1;
         }
         if(grid[r][c]==0){
-            paremeter++;
-            return;
+            return 1;
         }
         if(grid[r][c]==-1){
-            return;
+            return 0;
         }
 
         grid[r][c]=-1;
 
+        int paremeter = 0;
         for(auto &d: directions){
-            dfs(r+d[0], c+d[1], grid);
+            paremeter+=dfs(r+d[0], c+d[1], grid);
         }
+        return paremeter;
     }
     int islandPerimeter(vector<vector<int>>& grid) {
         n=grid.size();
         m=grid[0].size();
 
+        int paremeter = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]){
-                    dfs(i, j, grid);
-                    return paremeter;
+                    paremeter+=dfs(i, j, grid);
                 }
             }
         }
