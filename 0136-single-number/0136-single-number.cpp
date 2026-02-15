ade2db0c18841c7e704b12551cb34931f7d316cd
Mriganka <52573759+turtlebeasts@users.noreply.github.com>
2026-02-15 18:16:00 +0530

    Time: 12 ms (12.22%), Space: 23.8 MB (11.06%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // space complexity O(n) time complexity O(n)
     int singleNumber(vector<int>& nums) {
         unordered_map<int, int> freq;
         for(int i: nums){
@@ -11,4 +12,17 @@ public:
         }
         return -1;
     }
+    //time Complexity O(n^2) space complexity O(1)
+    // int singleNumber(vector<int>& nums) {
+    //     for(int i=0;i<nums.size();i++){
+    //         bool isUnique=true;
+    //         for(int j=0;j<nums.size();j++){
+    //             if(i!=j && nums[i]==nums[j]){
+    //                 isUnique=false;
+    //             }
+    //         }
+    //         if(isUnique) return nums[i];
+    //     }
+    //     return -1;
+    // }
 };
