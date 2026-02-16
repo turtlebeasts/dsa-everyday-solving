375ef236ee90bdcc00023f8911a0763170bd0ad3
Mriganka <52573759+turtlebeasts@users.noreply.github.com>
2026-02-16 21:27:37 +0530

    Time: 3 ms (31.14%), Space: 9.8 MB (77.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    string stripString(string s){
-        string result = "";
-        for(char c: s){
-            c=tolower(c);
-            if((c>='a' && c<='z')||(c>='0' && c<='9')){
-                result+=c;
-            }
-        }
-        return result;
-    }
+    
     bool isPalindrome(string s) {
-        string striped = stripString(s);
-        int i=0;
-        int j=striped.size()-1;
-        cout << striped;
+        int i=0, j=s.size()-1;
         while(i<j){
-            if(tolower(striped[i])==tolower(striped[j])){
+            if(!isalnum(s[i])){
+                i++;
+            }else if(!isalnum(s[j])){
+                j--;
+            }else if(tolower(s[i])==tolower(s[j])){
                 i++;
                 j--;
             }else{
