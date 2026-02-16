6c5db3fd74a6bd95d0461ebfbb1db9a0327e54f5
Mriganka <52573759+turtlebeasts@users.noreply.github.com>
2026-02-17 00:40:06 +0530

    Time: 1 ms (65.97%), Space: 11.6 MB (97.77%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> freq;
+        vector<int> freq(26,0);
 
         for(char c: ransomNote){
-            freq[c]++;
-        }
-        
-        for(char c: magazine){
-            freq[c]--;
+            freq[c-'a']++;
         }
 
-        for(auto &f: freq){
-            if(f.second>0) return false;
+        for(char c: magazine){
+            freq[c-'a']--;
+        }
+
+        for(int i: freq){
+            if(i>0) return false;
         }
 
         return true;
