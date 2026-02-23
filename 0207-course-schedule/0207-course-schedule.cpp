9d84a51be4dcf588b36636b64543261bfcc0cb8b
Mriganka <52573759+turtlebeasts@users.noreply.github.com>
2026-02-24 02:13:44 +0530

    Time: 3 ms (77.07%), Space: 19.1 MB (81.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& pathVisited){
-        visited[node]=true;
-        pathVisited[node]=true;
-        for(int neighbour: adj[node]){
-            if(!visited[neighbour]){
-                if(dfs(neighbour, adj, visited, pathVisited))
-                    return true;
-            }else if(pathVisited[neighbour]){
-                return true;
-            }
-        }
-        pathVisited[node]=false;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<bool> visited(numCourses, false);
-        vector<bool> pathVisited(numCourses, false);
+        vector<int> indegree(numCourses, 0);
+        queue<int> q;
 
         for(auto &e: prerequisites){
             adj[e[1]].push_back(e[0]);
+            indegree[e[0]]++;
         }
-        
-        for(int i = 0; i< numCourses; i++){
-            if(!visited[i]){
-                if(dfs(i, adj, visited, pathVisited)) return false;
+
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i]==0) q.push(i);
+        }
+
+        int processed = 0;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            processed++;
+            for(int v: adj[node]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.push(v);
+                }
             }
         }
-        return true;
+        return numCourses==processed;
     }
 };
