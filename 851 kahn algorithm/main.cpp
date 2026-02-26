854abf2aa41552bf9d73de47fad8d9a2f57011de
turtlebeasts <179turtleshells@gmail.com>
2026-02-26 18:00:00 +0530

    LeetCode solution

---
+++ 851 kahn algorithm/main.cpp
@@ -0,0 +1,56 @@
+class Solution
+{
+public:
+    vector<int> loudAndRich(vector<vector<int>> &richer, vector<int> &quiet)
+    {
+        int n = quiet.size();
+
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+
+        for (auto &r : richer)
+        {
+            int u = r[0];
+            int v = r[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] = i;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+        {
+            if (indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+
+        while (!q.empty())
+        {
+            int u = q.front();
+            q.pop();
+
+            for (int v : adj[u])
+            {
+                if (quiet[ans[u]] < quiet[ans[v]])
+                {
+                    ans[v] = ans[u];
+                }
+
+                indegree[v]--;
+                if (indegree[v] == 0)
+                {
+                    q.push(v);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
