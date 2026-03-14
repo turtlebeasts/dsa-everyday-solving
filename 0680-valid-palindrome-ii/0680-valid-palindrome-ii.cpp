2074b84cd42f4bbdf04c23147d49a5fd443f9a22
Mriganka <52573759+turtlebeasts@users.noreply.github.com>
2026-03-15 00:49:30 +0530

    Time: 3 ms (70.42%), Space: 22.1 MB (61.72%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    bool isPalindromeRange(string &s, int l, int r) {
-        while (l < r) {
-            if (s[l] != s[r]) return false;
+    bool isPalindrome(string& s, int l, int r){
+        while(l<r){
+            if(s[l]!=s[r]) return false;
             l++;
             r--;
         }
         return true;
     }
-
     bool validPalindrome(string s) {
-        int l = 0, r = s.size() - 1;
+        int l = 0;
+        int r = s.size()-1;
 
-        while (l < r) {
-            if (s[l] == s[r]) {
+        while(l<r){
+            if(s[l]==s[r]){
                 l++;
                 r--;
-            } else {
-                return isPalindromeRange(s, l + 1, r) ||
-                       isPalindromeRange(s, l, r - 1);
+            }else{
+                return isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1);
             }
         }
         return true;
